3e11189e605cb5baa5f7cb5d15e619a4c999c81a
KDKftw <ooo.kadoun@gmail.com>
2021-10-07 13:41:58 +0200

    srlv2 added, some try expect, email fnct to do

---
+++ complex_TestCase/SRL_complex.py
@@ -0,0 +1,124 @@
+from is_displayed_TestCase.to_import import acceptConsent, URL, URL_stat, caps, closeExponeaBanner
+from is_displayed_TestCase.to_import_secret import comandExecutor, sendEmail
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from threading import Thread
+
+
+##driver = webdriver.Chrome(executable_path=r"C:\Users\KADOUN\Desktop\Selenium setup\chromedriver94.exe")
+##driver = webdriver.Chrome(executable_path=r"C:\Users\KDK\Desktop\Selenium setup\chromedriver94.exe")
+URL_SRL = "https://www.fischer.cz/vysledky-vyhledavani?d=627|974|596|712|684|955&tt=1&to=4305|4309|2682|4308|4312&dd=2021-10-08&rd=2021-10-17&nn=7|8|9&m=5&ri=5&ac1=2"
+##URL_SRL = "https://www.eximtours.cz/vysledky-vyhledavani?tt=0&ac1=2&dd=2021-08-27&rd=2021-09-26&nn=7&d=63720|63719&pf=0&pt=900000"
+
+def SRLtestV2(desired_cap):
+    driver = webdriver.Remote(
+        command_executor=comandExecutor,
+        desired_capabilities=desired_cap)
+    x=0         ##variable for taking the first hotel, starting at 0
+    windowHandle = 1  ##variable for handling windows, gotta start on 1
+
+    driver.get(URL_SRL)
+    wait = WebDriverWait(driver, 150000)
+
+    time.sleep(2)
+    acceptConsent(driver)
+    time.sleep(2)
+    closeExponeaBanner(driver)
+
+    hotelyAllKarty = driver.find_elements_by_xpath("//*[@class='f_searchResult'and not(@style='display: none;')]//*[@class='f_searchResult-content-item']")
+    for WebElement in hotelyAllKarty:
+
+        terminZajezdu = driver.find_elements_by_xpath("//*[@class='f_tile f_tile--searchResultTour']//*[@class='f_list-item']")
+        terminZajezduSingle = driver.find_element_by_xpath("//*[@class='f_tile f_tile--searchResultTour']//*[@class='f_list-item']")
+
+        wait.until(EC.visibility_of(terminZajezduSingle))
+        ##print(terminZajezdu[x].text)
+
+        linkDetail = driver.find_elements_by_xpath("//*[@class='f_tile-priceDetail-item']/a")
+        linkDetailActualUrl = linkDetail[x].get_attribute("href")
+        ##print(linkDetailActualUrl)
+
+        stravaZajezdu = driver.find_elements_by_xpath("//*[@class='f_list-item f_icon f_icon--cutlery']")
+        stravaZajezduString = stravaZajezdu[x].text
+
+        pokojZajezdu = driver.find_elements_by_xpath("//*[@class='f_list-item f_icon f_icon--bed']")
+        pokojZajezduString = pokojZajezdu[x].text
+        ##print(pokojZajezduString)
+
+        cenaZajezduAll = driver.find_elements_by_xpath("//*[@class='f_tile-priceDetail-content']//*[@class='f_price']")
+        cenaZajezduAllString = cenaZajezduAll[x].text
+        ##print(cenaZajezduAllString)
+
+        cenaZajezduAdult = driver.find_elements_by_xpath("//*[@class='f_tile-priceDetail-item']//*[@class='f_tile-priceDetail-note'] //*[@class='f_price']")
+        cenaZajezduAdultString = cenaZajezduAdult[x].text
+        print(cenaZajezduAdultString)
+
+
+
+        driver.execute_script("window.open("");")
+        driver.switch_to.window(driver.window_handles[windowHandle])
+        driver.get(linkDetailActualUrl)
+
+        closeExponeaBanner(driver)
+
+        time.sleep(1)       ##natvrdo aby se to neposralo
+
+        detailTerminSedivka = driver.find_element_by_xpath("//*[@class='fshr-detail-summary-title']")
+        ##print(detailTerminSedivka.text)
+
+        detailStravaSedivka = driver.find_elements_by_xpath("//*[@class='fshr-detail-summary-paragraph']")
+        detailStravaSedivkaString = detailStravaSedivka[1].text         ##gottaa be 1 cuz thats how its set up (multiple locators are attached to this locator so position 1 is always gonna be strava hopefully
+
+        detailPokojSedivka = driver.find_element_by_xpath("//*[@class='fshr-detail-summary-title fshr-icon fshr-icon--bed']")
+        detailPokojSedivkaString = detailPokojSedivka.text
+        detailPokojSedivkaString = detailPokojSedivkaString[:-3]            ##need to be edited cuz there is random spaces and "?" in the element
+        ##print(detailPokojSedivkaString)
+
+        detailCenaAll = driver.find_element_by_xpath("//*[@class='fshr-tooltip-underline js-totalPrice']")
+        detailCenaAllString = detailCenaAll.text
+        ##print(detailCenaAllString)
+        try:
+            detailCenaAdult = driver.find_element_by_xpath('//*[contains(concat(" ", normalize-space(@class), " "), " fshr-detail-summary-price-header ")]//*[contains(concat(" ", normalize-space(@class), " "), " fshr-price ")]')
+            detailCenaAdultString = detailCenaAdult.text
+            print(detailCenaAdultString)
+
+        except NoSuchElementException:
+            pass
+
+
+        if detailPokojSedivkaString == pokojZajezduString:
+            print("pokoje sedí srl vs detail")
+        else:
+            print(" nesedí pokoj SRL vs sedivka")
+
+        if detailStravaSedivkaString == stravaZajezduString:
+            print("stravy sedí srl vs detail")
+
+        else:
+            print( "nesedí strava srl vs ssedika")
+
+        if detailCenaAllString == cenaZajezduAllString:
+            print ("ceny all sedí srl vs detail")
+
+        else:
+            print("ceny all problem srl vs detail")
+
+        if detailCenaAdultString == cenaZajezduAdultString:
+            print(" cena adult sedí srl vs detail")
+
+        else:
+            print("cena adult nesedi srl vs detail")
+
+        driver.switch_to.window(driver.window_handles[0])   ##this gotta be adjusted based on what test is executed
+        ##for daily test needs to be set on 1 so it gets on the SRL
+
+        x = x +1
+        print(x)
+        windowHandle = windowHandle + 1
+        print(windowHandle)
+
+for cap in caps:
+        Thread(target=SRLtestV2, args=(cap,)).start()
--- is_displayed_TestCase/to_import.py
+++ is_displayed_TestCase/to_import.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 URL = "https://www.eximtours.cz"
 URL_faq = URL+"/faq"
@@ -10,7 +11,7 @@ URL_fm = URL+"/first-minute"
 URL_fmExotika = URL+"/first-minute/zima"
 URL_detail = URL+"/turecko/turecka-riviera/alanya/kleopatra-royal-palm?DS=1&GIATA=38694&D=627|974|596|712|684|955&HID=129375&MT=5&DI=13&RT=15&NN=7&RD=2021-09-25&DD=2021-09-18&DP=4305&TO=4305|4309|2682|4308|4312&MNN=7&TT=1&PID=AYPAL&DPR=Fischer&TTM=1&TOM=4305|4309|2682|4308|4312&DF=2021-09-18|2021-10-19&ERM=0&NNM=7&ac1=2&kc1=0&ic1=0"
 
-caps=[{
+caps2=[{
       'os_version': 'Big Sur',
       'os': 'OS X',
       'browser': 'safari',
@@ -19,7 +20,23 @@ caps=[{
       'build': 'browserstack-build-1'
       }]
 
+caps3 = [{
+      'os_version': '10',
+      'os': 'Windows',
+      'browser': 'ie',
+      'browser_version': '11.0',
+      'name': 'Parallel Test2',
+      'build': 'browserstack-build-1'
+      }]
 
+caps = [   {
+      'os_version': '10',
+      'os': 'Windows',
+      'browser': 'chrome',
+      'browser_version': '94.0',
+      'name': 'Parallel Test2',
+      'build': 'browserstack-build-1'
+      }]
 
 def acceptConsent(driver):
     def expand_shadow_element(element):
@@ -32,12 +49,18 @@ def acceptConsent(driver):
     except NoSuchElementException:
         pass
 
-def closePopupBanner(driver):
+def closeExponeaBanner(driver):
+    time.sleep(1.5)
+    wait = WebDriverWait(driver, 150000)
+    driver.maximize_window()
     try:
-        wait = WebDriverWait(driver, 150000)
-        bannerClose = driver.find_element_by_xpath('//*[@class="close"]')
-        wait.until(EC.visibility_of(bannerClose))
-        bannerClose.click()
+        exponeaBanner = driver.find_element_by_xpath("//*[@class='exponea-popup-banner']")
+        if exponeaBanner.is_displayed():
+
+            wait.until(EC.visibility_of(exponeaBanner))
+            exponeaCrossAndBanner = driver.find_element_by_xpath("//*[@class='exponea-popup-banner']//*[@class='exponea-close']")
+            exponeaCrossAndBanner.click()
+            time.sleep(2)
 
     except NoSuchElementException:
-        pass
+        print( "nenasle se exponea banner")
